2d3ce91552eb27064256f91ce3c41ee502fc1900
rootv890 <imakeitreal@icloud.com>
2024-04-22 23:40:28 +0530

    Algorithm Bubble Sort Doc fix

--- algorithm/bubble_sort.py
+++ algorithm/bubble_sort.py
@@ -1,6 +1,12 @@
 """ Simple algorithm to sort things by swapping position using Bubble Sort Theorm
     Big-O complexity ==> O(n^2)
-    The algorithm is pretty simple: compare two items in an array that are next to each other. If they're out of order (that is, the larger one comes first in the array) swap them. Then move forward one index, compare again, swap if needed, and continue to the next item in the array. Once we've reached the end of the array, if we've swapped anything in the previous run through, the array could still be out of order, so we have to pass through again. Once we run through the whole array with no swaps, the array is sorted!
+    What's this Algorith about ?
+    The algorithm is pretty simple: compare two items in an array that are next to each other.
+      If they're out of order (that is, the larger one comes first in the array) swap them. 
+      Then move forward one index, compare again, swap if needed, and continue to the next item in the array. 
+      Once we've reached the end of the array, if we've swapped anything in the previous run through, 
+      the array could still be out of order, so we have to pass through again. Once we run through 
+      the whole array with no swaps, the array is sorted!
  """
 
 
